10ee26d263702f2d6c42cbc64ecdc9b2742865cb
mcorusa <marinacorusa1@gmail.com>
2024-04-08 14:19:41 +0200

    attackBox drawing

--- js/classes.js
+++ js/classes.js
@@ -110,12 +110,12 @@ class Fighter extends Sprite {
     this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
 
     //draw the attack box
-    c.fillRect(
-      this.attackBox.position.x,
-      this.attackBox.position.y,
-      this.attackBox.width,
-      this.attackBox.height
-    );
+    // c.fillRect(
+    //   this.attackBox.position.x,
+    //   this.attackBox.position.y,
+    //   this.attackBox.width,
+    //   this.attackBox.height
+    // );
 
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y; // za svaki frame u loopu na y ce se dodavati 10px
